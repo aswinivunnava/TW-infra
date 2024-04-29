9379717ce5d5107a826d3736d62bd0a8cbde8ee0
aswinivunnava <90748707+aswinivunnava@users.noreply.github.com>
2024-04-29 18:00:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,18 +14,18 @@ provider "azurerm" {
 }
 
 data "azurerm_resource_group" "RG1" {
-  name     = "RG1-resources"
+  name     = "${var.prefix}-RG1"
 }
 
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "vnet1-network"
+  name                = "${var.prefix}-vnet1"
   resource_group_name = data.azurerm_resource_group.RG1.name
   location = data.azurerm_resource_group.RG1.location
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "subnet1" {
-  name                 = "subnet1"
+  name                 = "${var.prefix}-subnet1"
   resource_group_name  = data.azurerm_resource_group.RG1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.1.0/24"]
