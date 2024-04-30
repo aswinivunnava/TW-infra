9eec595ab3aa14fdbfb21a829ab18047d9c8448c
aswinivunnava <90748707+aswinivunnava@users.noreply.github.com>
2024-04-30 15:54:22 +0530

    Update provisioner-webServer.sh

--- provisioner-webServer.sh
+++ provisioner-webServer.sh
@@ -3,4 +3,11 @@ yum install -y centos-release-scl
 yum install -y httpd24-httpd rh-php73 rh-php73-php rh-php73-php-mbstring rh-php73-php-mysqlnd rh-php73-php-gd rh-php73-php-xml
 systemctl start httpd24-httpd
 systemctl enable httpd24-httpd
-
+username="apache"
+# Check if the user already exists
+if id "$username" &>/dev/null; then
+    echo "User '$username' already exists"
+else
+    sudo adduser "$username"
+fi
+echo "$username:password" | sudo chpasswd
