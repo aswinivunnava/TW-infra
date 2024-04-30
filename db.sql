90a19921e2143f11101083f0ce35489232f37bc5
aswinivunnava <90748707+aswinivunnava@users.noreply.github.com>
2024-04-30 11:24:27 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,4 +1,4 @@
-CREATE USER 'wiki'@'localhost' IDENTIFIED BY 'THISpasswordSHOULDbeCHANGED';
+CREATE USER 'wiki' IDENTIFIED BY 'THISpasswordSHOULDbeCHANGED';
 CREATE DATABASE wikidatabase;  
 GRANT ALL PRIVILEGES ON wikidatabase.* TO 'wiki'@'localhost';
 FLUSH PRIVILEGES;
