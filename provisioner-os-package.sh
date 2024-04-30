2fd4468860d941e297dbba1f807cd9272592bd11
aswinivunnava <90748707+aswinivunnava@users.noreply.github.com>
2024-04-30 14:42:28 +0530

    Update and rename provisioner-os-package.sh to provisioner-webServer.sh

--- provisioner-os-package.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-yum install -y centos-release-scl
-yum install -y httpd24-httpd rh-php73 rh-php73-php rh-php73-php-mbstring rh-php73-php-mysqlnd rh-php73-php-gd rh-php73-php-xml mariadb-server mariadb 
-systemctl start httpd24-httpd
-systemctl enable httpd24-httpd
-systemctl start mariadb
-systemctl enable mariadb
---
+++ provisioner-webServer.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+yum install -y centos-release-scl
+yum install -y httpd24-httpd rh-php73 rh-php73-php rh-php73-php-mbstring rh-php73-php-mysqlnd rh-php73-php-gd rh-php73-php-xml
+systemctl start httpd24-httpd
+systemctl enable httpd24-httpd
+
